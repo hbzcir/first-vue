ba78d96dea0faaf9903f93a87cb677a90faf8712
Eun-Young-Lee <hbzcir@naver.com>
2018-07-23 01:32:40 +0900

    todoApp 전역 지역 등록 테스트
    test 60p (doit vue)
    todo app list, footer component add

---
+++ js/app.js
@@ -0,0 +1,18 @@
+// todo-footer 컴포넌트 전역 등록 
+
+
+Vue.component('todo-footer',{
+    template : '<footer> Footer Area </footer>'
+})
+var todoList = {
+    template : '<p> List Area </p>'
+} 
+var app = new Vue({
+    el: '#app',
+    data: {
+        message: 'This is a parent component'
+    },
+    components: {
+        'todo-list' : todoList
+    }
+});
